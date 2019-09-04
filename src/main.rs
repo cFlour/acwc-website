eca4999520d884f2708b4cbe9098422665b8d0c4
Thomas Daniels <daniels.thomas@pm.me>
2019-09-04 22:27:26 +0200

    Time-based homepage view

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,40 @@
 #[macro_use]
 extern crate rocket;
 
+use chrono::prelude::*;
 use rocket_contrib::templates::Template;
+
+fn empty_context() -> std::collections::HashMap::<u8, u8> {
+    std::collections::HashMap::<u8, u8>::new()
+}
+
+fn registration_state() -> i32 {
+    let register_start = Utc.ymd(2019, 9, 5).and_hms(20, 0, 0);
+    let register_end = Utc.ymd(2019, 9, 27).and_hms(20, 0, 0);
+    let now = Utc::now();
+    if now >= register_end {
+        2 // registration ended
+    } else if now >= register_start {
+        1 // registration open
+    } else {
+        0 // registration not yet open
+    }
+}
 
 #[get("/")]
 fn home() -> Template {
-    Template::render("home", &std::collections::HashMap::<u8, u8>::new())
+    match registration_state() {
+        0 => Template::render("home", &empty_context()),
+        1 => Template::render("home_registrationopen", &empty_context()),
+        2 => Template::render("home_registrationclosed", &empty_context()),
+        _ => unreachable!(),
+    }
+
 }
 
 #[get("/rules/2019")]
 fn rules_2019() -> Template {
-    Template::render("rules2019", &std::collections::HashMap::<u8, u8>::new())
+    Template::render("rules2019", &empty_context())
 }
 
 fn main() {
